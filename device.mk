5e07ec9064c4e8c54133a593598bf4dd626f0798
aayowtf <atharv2951@proton.me>
2024-03-18 04:59:59 +0530

    ceres: Define partitions
    Signed-off-by: aayowtf <atharv2951@proton.me>

--- device.mk
+++ device.mk
@@ -4,8 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
